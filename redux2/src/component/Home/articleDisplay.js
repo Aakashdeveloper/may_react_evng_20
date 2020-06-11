81758c8021a59719f9536eea5733a1c82f9d7e15
aakashDeveloper <ahanda205@gmail.com>
2020-06-11 21:53:51 +0530

    a

---
+++ redux2/src/component/Home/articleDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ArticleDisplay = () => {
+    return(<h1>ArticleDisplay</h1>)
+}
+
+export default ArticleDisplay;
---
+++ redux2/src/component/Home/galleryDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GalleryDisplay = () => {
+    return(<h1>GalleryDisplay</h1>)
+}
+
+export default GalleryDisplay;
